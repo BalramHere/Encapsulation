5a2c03aa8005648b1cf843fb202d00307057f7b9
bnenavath <bnenavath@BNENAVAT-L-3324.evoketechnologies.com>
2023-02-28 19:35:00 +0530

    Second Commit

--- src/Encap.java
+++ src/Encap.java
@@ -9,6 +9,6 @@ public class Encap {
 		System.out.println("Encap dept : "+a.dept);
 		//a.setName("Ram");
 		System.out.println("ENcap name :"+ a.getName());
-		
+		System.out.println("Hellllooooooo GIT");	
 	}
 }
