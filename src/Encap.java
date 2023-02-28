eef607d347f8147a79f31f5edabbe12ffe390ab0
bnenavath <bnenavath@BNENAVAT-L-3324.evoketechnologies.com>
2023-02-28 19:17:29 +0530

    Initial commit

---
+++ src/Encap.java
@@ -0,0 +1,14 @@
+
+public class Encap {
+
+	public static void main(String[] args)
+	{
+		Encapsulation a = new Encapsulation();
+		
+		a.dept="EEE";
+		System.out.println("Encap dept : "+a.dept);
+		//a.setName("Ram");
+		System.out.println("ENcap name :"+ a.getName());
+		
+	}
+}
---
+++ src/Encapsulation.java
@@ -0,0 +1,38 @@
+
+class Encapsulation {
+
+
+		private String name;
+		private int rollno;
+		
+		public String dept;
+		
+		public String getName() {
+			return name;
+		}
+
+		/*
+		 * public void setName(String name) { this.name = name; }
+		 */
+		public int getRollno() {
+			return rollno;
+		}
+		public void setRollno(int rollno) {
+			this.rollno = rollno;
+		}
+		
+	
+	public static void main(String[] args){
+		
+		Encapsulation a = new Encapsulation();
+		//a.setName("Balram");
+		//a.setName("Chinna");
+		a.dept="CSE";
+		a.setRollno(10);
+		System.out.println("Dept :: "+a.dept);
+		System.out.println("Name :: "+ a.getName());
+		System.out.println("Rollno :: "+ a.getRollno());
+	}
+	
+	
+}
